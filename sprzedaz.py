2979a5e4f60b6db69500909cbe10d543a9b311f8
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2022-01-23 21:19:45 +0100

    Update sprzedaz.py
    sprzedaz.py

--- sprzedaz.py
+++ sprzedaz.py
@@ -6,6 +6,7 @@ def main():
     if action == "sprzedaz" and len(sys.argv) == 5:
         manager.transform_data()
         manager.execute(action, sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+        # print(manager.error[0])
         manager.write_data_to_file(sys.argv[1])
         # manager.print_info("sprzedaz")
         manager.execute("konto")
